b85d61d9f5f077bf4987ebac7e15142f4792a2d4
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2023-07-20 23:03:57 +0200

    Updated script
    Added option to search an website on waybackmachine from date to date.

--- wayback.py
+++ wayback.py
@@ -20,11 +20,11 @@ print(red, '\n'
 
 if len(sys.argv) > 1:
     wayback_api = "https://web.archive.org/cdx/search/cdx?url="
-    if len(sys.argv) > 2:
-        wayback_date = "&from=" + str(sys.argv[2]) + "&to=" + str(sys.argv[2]) + "&gzip=false"
+    if len(sys.argv) > 3:
+        wayback_date = "&from=" + str(sys.argv[2]) + "&to=" + str(sys.argv[3]) + "&gzip=false"
     else:
-        print(red, 'Error:', white, 'Missing date to search web archives on wayback')
-        print(red, 'Error:', white, 'Valid date input is in YYYY format. Example 2023')
+        print(red, 'Error:', white, 'Missing DATE-FROM DATE-TO')
+        print(red, 'Example:', white, './wayback.py www.google.com 2020 2023')
         sys.exit(1)
 
     print(red, "NOTICE:", white, "This might take a while.. Go grab a coffee!!")
@@ -34,5 +34,6 @@ if len(sys.argv) > 1:
     wayback = requests.request('get', full_url)
     print(wayback.text)
 else:
-    print(red, 'Error:', white, 'Missing url to search for on wayback')
-
+    print(red, 'Error:', white, 'Invalid usage....')
+    print(red, 'Error:', white, 'To search google.com from 2020 to 2023 use following example')
+    print(red, 'Example usage:', white, './wayback.py www.google.com 2020 2023')
